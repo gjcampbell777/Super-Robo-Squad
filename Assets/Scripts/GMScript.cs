ba1704d878757b92b240f9e8fd912a55fd0cd782
Gregory Campbell <gjcampbell777@gmail.com>
2020-04-28 20:25:18 -0400

    Fixed some bugs and UI overlap caused by the restart and cursor lock additions

--- Assets/Scripts/GMScript.cs
+++ Assets/Scripts/GMScript.cs
@@ -195,6 +195,22 @@ public class GMScript : MonoBehaviour
 
         }
 
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        	Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		    Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+		    RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+        	
+	        if (hit.collider != null) {
+		    	if(hit.collider.gameObject.tag == "Restart")
+		    	{
+		    		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		    	}
+			}
+		}
+
         if(cursorLock)
         {
 
