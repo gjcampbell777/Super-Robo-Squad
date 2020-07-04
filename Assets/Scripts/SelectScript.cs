a47020f2f57c5ded896935f8d54b38ab637fad0d
Gregory Campbell <gjcampbell777@gmail.com>
2020-07-04 13:28:24 -0400

    Added side trim to the mode selection scene when a mouse hovers over a game mode selection with the mouse

--- Assets/Scripts/SelectScript.cs
+++ Assets/Scripts/SelectScript.cs
@@ -6,8 +6,12 @@ using UnityEngine.SceneManagement;
 public class SelectScript : MonoBehaviour
 {
 
+	public Sprite[] SideTrim;
+
 	private int gameMode;
 	private int level;
+	private GameObject SideTrimObject;
+	private GameObject SideTrimObjectFlip;
 
     // Start is called before the first frame update
     void Start()
@@ -18,51 +22,100 @@ public class SelectScript : MonoBehaviour
     	PlayerPrefs.SetInt("GameMode", gameMode);
     	PlayerPrefs.SetInt("Level", level);
 
+    	SideTrimObject = new GameObject("SideTrimLeft");
+		SideTrimObject.AddComponent<SpriteRenderer>();
+		SideTrimObject.transform.position = new Vector3(-5.5f, 0f, 0f);
+		SideTrimObject.SetActive(false);
+		SideTrimObject.tag = "SideTrim";
+
+		SideTrimObjectFlip = new GameObject("SideTrimRight");
+		SideTrimObjectFlip.AddComponent<SpriteRenderer>();
+		SideTrimObjectFlip.transform.position = new Vector3(5.5f, 0f, 0f);
+		SideTrimObjectFlip.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+		SideTrimObjectFlip.SetActive(false);
+		SideTrimObjectFlip.tag = "SideTrim";
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-	    if (Input.GetMouseButtonDown(0))
-	    {
+    	Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	    Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
 
-	    	Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		    Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+	    RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+    	
+        if (hit.collider != null) {
 
-		    RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-	    	
-	        if (hit.collider != null) {
+        	SpriteRenderer renderer = SideTrimObject.GetComponent<SpriteRenderer>();
+        	SpriteRenderer rendererFlip = SideTrimObjectFlip.GetComponent<SpriteRenderer>();
 		    	
-		    	if(hit.collider.gameObject.tag == "Story")
-		    	{
+	    	if(hit.collider.gameObject.tag == "Story")
+	    	{
+
+	    		renderer.sprite = SideTrim[0];
+	    		rendererFlip.sprite = SideTrim[0];
+
+	    		if (Input.GetMouseButtonDown(0))
+	    		{
 		    		gameMode = 0;
 		    		PlayerPrefs.SetInt("GameMode", gameMode);
 		    		SceneManager.LoadScene("Battle Scene");
 		    	}
+	    	}
 
-		    	if(hit.collider.gameObject.tag == "Random")
-		    	{
-		    		gameMode = 1;
+	    	if(hit.collider.gameObject.tag == "Random")
+	    	{
+
+	    		renderer.sprite = SideTrim[1];
+	    		rendererFlip.sprite = SideTrim[1];
+
+	    		if (Input.GetMouseButtonDown(0))
+	    		{
+	    			gameMode = 1;
 		    		PlayerPrefs.SetInt("GameMode", gameMode);
 		    		SceneManager.LoadScene("Battle Scene");
 		    	}
+	    	}
 
-		    	if(hit.collider.gameObject.tag == "Challenge")
-		    	{
-		    		gameMode = 2;
+	    	if(hit.collider.gameObject.tag == "Challenge")
+	    	{
+
+	    		renderer.sprite = SideTrim[2];
+	    		rendererFlip.sprite = SideTrim[2];
+
+	    		if (Input.GetMouseButtonDown(0))
+	    		{
+	    			gameMode = 2;
 		    		PlayerPrefs.SetInt("GameMode", gameMode);
 		    		SceneManager.LoadScene("Battle Scene");
 		    	}
+	    	}
 
-		    	if(hit.collider.gameObject.tag == "Training")
-		    	{
+	    	if(hit.collider.gameObject.tag == "Training")
+	    	{
+
+	    		renderer.sprite = SideTrim[3];
+	    		rendererFlip.sprite = SideTrim[3];
+
+	    		if (Input.GetMouseButtonDown(0))
+	    		{
 		    		gameMode = 3;
 		    		PlayerPrefs.SetInt("GameMode", gameMode);
 		    		SceneManager.LoadScene("Battle Scene");
 		    	}
+	    	}
 
-			}
+			SideTrimObject.SetActive(true);
+			SideTrimObjectFlip.SetActive(true);
+
+		} else {
+
+			GameObject[] sideTrims = GameObject.FindGameObjectsWithTag("SideTrim");
+   			foreach(GameObject sidetrim in sideTrims)
+   			sidetrim.SetActive(false);
+
 		}
 
     }
