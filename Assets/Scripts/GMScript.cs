7a095cc25bed2e5e2211e45ec4c29814fcbbcf7b
Gregory Campbell <gjcampbell777@gmail.com>
2020-07-22 20:28:11 -0400

    Removed dead air from mode selection audio and removed commented out code

--- Assets/Scripts/GMScript.cs
+++ Assets/Scripts/GMScript.cs
@@ -1090,8 +1090,6 @@ public class GMScript : MonoBehaviour
 		yield return new WaitForSeconds(
 			enemyAttackAnim.GetCurrentAnimatorStateInfo(0).length*0.45f);
 
-		//enemyAttackAnim.SetTrigger("AttackTrigger");
-
     }
 
     IEnumerator PreAttackBuffAnimEnemy()
